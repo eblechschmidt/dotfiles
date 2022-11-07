7b0372223232dff8b320011342752e072861af84
Eike Blechschmidt <eike@eikeb.de>
2022-11-07 21:05:02 +0100

    bug fix: files were in wrong folder

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,4 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.telescope"
+require "eike.options"
+require "eike.keymaps"
+require "eike.plugins"
+require "eike.telescope"
