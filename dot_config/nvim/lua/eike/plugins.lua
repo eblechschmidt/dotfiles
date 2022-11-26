587022f8faa4f5805a9583fd36f3d921ae43d9a5
Eike Blechschmidt <eike@eikeb.de>
2022-11-26 13:39:02 +0100

    adds whichkey to nvim config

--- dot_config/nvim/lua/eike/plugins.lua
+++ dot_config/nvim/lua/eike/plugins.lua
@@ -48,7 +48,8 @@ return packer.startup(function(use)
   -- Telescope
   use "nvim-lua/plenary.nvim"
   use "nvim-telescope/telescope.nvim"
-
+  -- Show keys
+  use "folke/which-key.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
