5323162de0790b70fcc2d568b94e2c39d017b370
Eike Blechschmidt <eike@eikeb.de>
2022-12-02 19:24:54 +0100

    adds automatic imports and gofmt

--- dot_config/nvim/lua/eike/lsp/init.lua
+++ dot_config/nvim/lua/eike/lsp/init.lua
@@ -3,10 +3,34 @@ if not status_ok then
   return
 end
 
--- require "user.lsp.lsp-signature"
 require "eike.lsp.lsp-installer"
 require("eike.lsp.handlers").setup()
 require "eike.lsp.null-ls"
 require "eike.lsp.lsp-signature"
 
-vim.cmd[[autocmd BufWritePre *.go lua vim.lsp.buf.formatting()]]
+-- auto import for go
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		local params = vim.lsp.util.make_range_params(nil, "utf-16")
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end,
+})
+
+-- gofmt
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		vim.lsp.buf.formatting_sync(nil, 500)
+	end,
+})
